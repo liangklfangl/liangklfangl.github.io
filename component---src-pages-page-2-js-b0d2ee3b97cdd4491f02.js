1d08af9ab8b2545551568b7f1e4b6a6697de3f42
qinliang.ql <qinliang.ql@alibaba-inc.com>
2019-10-30 19:31:53 +0800

    Updates

---
+++ component---src-pages-page-2-js-b0d2ee3b97cdd4491f02.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{p5nM:function(e,t,a){"use strict";a.r(t);var n=a("OkUi"),l=a.n(n),o=a("Wbzz"),c=a("Bl7J"),r=a("vrFN");t.default=function(){return l.a.createElement(c.a,null,l.a.createElement(r.a,{title:"Page two"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2"),l.a.createElement(o.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-b0d2ee3b97cdd4491f02.js.map
