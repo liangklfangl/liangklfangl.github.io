da17d2a296403122c31993b64889b21a0bd5efb5
liangklfang <liangklfang@163.com>
2019-10-30 20:01:39 +0800

    新增部分捷荣

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,10 +5,11 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
-  
+
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,4 @@
  */
 
 // You can delete this file if you're not using it
+// 新增部分捷荣
